5e144cce5f05462efb80af9568b7af1c794aaa58
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2022-12-14 02:03:51 +0700

    work with handlers

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,11 +2,51 @@ package ru.netology;
 
 import ru.netology.server.Server;
 
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
-    Server server = new Server();
-    server.run();
-  }
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+
+    public static void main(String[] args) {
+        Server.addHandler(GET, "/index.html", Server::processFile);
+        Server.addHandler(GET, "/spring.svg", Server::processFile);
+        Server.addHandler(GET, "/spring.png", Server::processFile);
+        Server.addHandler(GET, "/resources.html", Server::processFile);
+        Server.addHandler(GET, "/styles.css", Server::processFile);
+        Server.addHandler(GET, "/app.js", Server::processFile);
+        Server.addHandler(GET, "/links.html", Server::processFile);
+        Server.addHandler(GET, "/forms.html", Server::processFile);
+        Server.addHandler(GET, "/events.html", Server::processFile);
+        Server.addHandler(GET, "/events.js", Server::processFile);
+        Server.addHandler(GET, "/internalError.html", Server::processFile);
+        Server.addHandler(GET, "/styles_internal.css", Server::processFile);
+        Server.addHandler(GET, "/classic.html", Server::processFileClassic);
+        Server.addHandler(GET, "/wrongMethod.html", Server::processFileClassic);
+        Server.addHandler(GET, "/messages", (request, out) -> {
+            var message = String.format("This is %s message", request.getMethod());
+            try {
+                Server.sc_ok(out, "text/plain", message.length(), message);
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Server.addHandler(POST, "/messages", (request, out) -> {
+            var message = String.format("This is %s message", request.getMethod());
+            try {
+                Server.sc_ok(out, "text/plain", message.length(), message);
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        
+        var server = new Server();
+        server.run();
+
+    }
 }
 
 
--- src/main/java/ru/netology/server/SingleThreadRunner.java
+++ src/main/java/ru/netology/server/SingleThreadRunner.java
@@ -5,95 +5,60 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
 
-public class SingleThreadRunner implements Runnable{
-    private Socket socket;
-    private List<String> validPaths = List.of("/index.html",
-            "/spring.svg", "/spring.png", "/resources.html",
-            "/styles.css", "/app.js", "/links.html",
-            "/forms.html", "/classic.html", "/events.html",
-            "/events.js");
-    private final int DEFAULT_PAGE_INDEX = 0;
-    SingleThreadRunner(Socket socket){
+public class SingleThreadRunner implements Runnable {
+    private final Socket socket;
+    private final String INTERNAL_SERVER_PAGE = "/internalError.html";
+    private final String NO_FOUND_PAGE = "/noFoundPage.html";
+    private final String NO_ALLOWED_REQUEST = "/wrongMethod.html";
+
+    SingleThreadRunner(Socket socket) {
         this.socket = socket;
+    }
+
+    void badRequest(BufferedOutputStream out) throws IOException {
+        Server.processFile(new Request(null, INTERNAL_SERVER_PAGE), out);
+    }
+
+    void notFoundRequest(BufferedOutputStream out) throws IOException {
+        Server.processFile(new Request(null, NO_FOUND_PAGE), out);
+    }
+
+    void notAllowedRequest(Request request, BufferedOutputStream out) throws IOException {
+        Server.processNotAllowed(new Request(request.getMethod(), NO_ALLOWED_REQUEST), out);
     }
 
     @Override
     public void run() {
 
         try (
-            final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            final var out = new BufferedOutputStream(socket.getOutputStream());
-            ) {
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream())
+        ) {
 
-                // read only request line for simplicity
-                // must be in form GET /path HTTP/1.1
-                final var requestLine = in.readLine();
-                final var parts = requestLine.split(" ");
+            var requestLine = in.readLine();
+            var requestData = requestLine.split(" ");
 
-                if (parts.length != 3) {
-                    // just close socket
-                    return;
+            System.out.println(requestData[0] + " " + requestData[1]);
+
+            if (requestData.length != 3) {
+                badRequest(out);
+            } else {
+
+                Request request = new Request(requestData[0], requestData[1]);
+
+                if (Server.handlers.containsKey(request.getPath())) {
+                    if (Server.handlers.get(request.getPath()).containsKey(request.getMethod())) {
+                        Server.handlers.get(request.getPath()).get(request.getMethod()).handle(request, out);
+                    } else {
+                        notAllowedRequest(request, out);
+                    }
+                } else {
+                    notFoundRequest(out);
                 }
-
-                final String path;
-
-                // if it connects to localhost:port, it will redirect to default page
-                if (parts[1].compareToIgnoreCase("") == 1){
-                    path = validPaths.get(DEFAULT_PAGE_INDEX);
-                }else{
-                    path = parts[1];
-                }
-
-                if (!validPaths.contains(path)) {
-                    out.write((
-                            "HTTP/1.1 404 Not Found\r\n" +
-                                    "Content-Length: 0\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    out.flush();
-                    return;
-                }
-
-                final var filePath = Path.of(".", "public", path);
-                final var mimeType = Files.probeContentType(filePath);
-
-                // special case for classic
-                if (path.equals("/classic.html")) {
-                    final var template = Files.readString(filePath);
-                    final var content = template.replace(
-                            "{time}",
-                            LocalDateTime.now().toString()
-                    ).getBytes();
-                    out.write((
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: " + mimeType + "\r\n" +
-                                    "Content-Length: " + content.length + "\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    out.write(content);
-                    out.flush();
-                    return;
-                }
-
-                final var length = Files.size(filePath);
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                Files.copy(filePath, out);
-                out.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
